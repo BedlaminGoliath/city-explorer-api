543c0284ecaee9dbf6f492aab76013c503992d37
christopher lopez <christopherwlopez89@gmail.com>
2022-10-17 20:35:53 -0700

    created server for movie

--- server.js
+++ server.js
@@ -4,8 +4,9 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const weather = require('./data/weather.json');
-// axios = require('axios'); when not working out of a json file
+// const weather = require('./data/weather.json');
+const axios = require('axios');
+
 
 //  create instance of an express server
 const app = express();
@@ -28,71 +29,72 @@ app.get('/', (request, response) => {
 //   response.status(200).send(doesntExist);
 
 // });
-app.get('/weather', handleWeatherFunction);
+
+
+// ===========TA help===================
+
+// app.get('/weather', handleWeatherFunction);
+
+// ============TA help===================
+
+
 // function handleWeatherFunction ( req, res, next){
 //   const searchQuery = req.query;
 //   console.log(req.query.searchQuery);
 
 // // defines endpoint that gets data and returns it to react
-// // app.get('/weather', (req, res, next) => {
-//   try {
+app.get('/weather', async (request, response, next) => {
+  try {
 
-//     // grabing the search query from the request object
-//     // notice the query parameter is named "type"
-//     // type is the name of the query paramter we must send along with axios from react in order to ask for data for our server
-//     // const lat = req.query.lat;
-//     // const lon = req.query.lon;
-//     // const { lat, lon, searchQuery } = request.query;
-//     console.log('query parameter: ', req.query);
-//     console.log(searchQuery);
-//     //   we need to have this
-//     const foreCastData = new ForeCast(searchQuery);
-//     const foreCastArray = foreCastData.getForeCast();
-//     res.status(200).send(foreCastArray);
-//   } catch (error) {
-//     next(error);
-//   }
-// }
+    //     // grabing the search query from the request object
+    //     // notice the query parameter is named "type"
+    //     // type is the name of the query paramter we must send along with axios from react in order to ask for data for our server
+    //     // const lat = req.query.lat;
+    //     // const lon = req.query.lon;
+    const { lat, lon, searchQuery } = request.query;
+    const url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&days=7&lat=${lat}&lon=${lon}`;
+    const weatherResponse = await axios.get(url);
+    console.log(weatherResponse.data);
+    const foreCastArray = weatherResponse.data.data.map(day => new ForeCast(day));
+    response.status(200).send(foreCastArray);
+  } catch (error) {
+    next(error);
+  }
+});
 
+class ForeCast {
+  constructor(day) {
+    this.date = day.datetime,
+    this.description = day.weather.description;
+  }
+}
+app.get('/movies', async (request, response, next) => {
+  try {
 
-// class ForeCast {
-//   constructor(searchQuery){
-//     // find method to find type of list we want to return
-//     console.log(weather);
-//     console.log(data);
-//     let data  = weather.find( city => city.city_name.toLowerCase() === searchQuery.toLowerCase());
-//     this.data = data;
-//     console.log(searchQuery);
+    const searchQuery  = request.query.searchQuery;
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIES_API_KEY}&query=${searchQuery}`;
+    const movies = await axios.get(url);
+    console.log(movies.data);
+    const moviesArray = movies.data.results.map(movie => new Movie(movie));
+    response.status(200).send(moviesArray);
+  } catch (error) {
+    next(error);
+  }
+});
 
-//   }
-
-function handleWeatherFunction(req, res, next){
-  let { searchQuery } = req.query;
-  let city = weather.find(city => city.city_name === searchQuery);
-  try{
-    const weatherArr = city.data.map( day => new ForeCast(day));
-    res.status(200).send(weatherArr);
-
-  } catch (err) {
-    next(err);
+class Movie {
+  constructor(movie) {
+    this.title = movie.original_title;
+    this.overview = movie.overview;
+    this.average_votes = movie.vote_average;
+    this.total_votes = movie.vote_count;
+    this.image_url = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+    this.popularity = movie.popularity;
+    this.released_on = movie.release_date;
   }
 }
 
-function ForeCast(day) {
-  this.date = day.valid_date;
-  this.description = day.weather.description;
-}
-//   a method that gets name and desc properties from itemValues array
-//   getForeCast(){
-//     return this.data.map( day => ({
-//       date: day.valid_date,
-//       description: day.weather.description
-//     }));
-//   }
-
-// }
-
-app.use((error, request, response, next ) => {
+app.use((error, request, response, next) => {
   console.log(error);
   response.status(500).send(error.message);
 });
