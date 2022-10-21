7b5c9f2d05468f5b5aae44cfe7062e674c744016
christopher lopez <christopherwlopez89@gmail.com>
2022-10-20 20:29:56 -0700

    cache

--- modules/movies.js
+++ modules/movies.js
@@ -1,15 +1,28 @@
 'use strict';
 
 const axios = require('axios');
+const cacheDB = require('./cache');
 
 const getMovies = async (request, response, next) => {
   try {
-    const searchQuery  = request.query.searchQuery;
+    const searchQuery = request.query.searchQuery;
     const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIES_API_KEY}&query=${searchQuery}`;
-    const movies = await axios.get(url);
-    console.log(movies.data);
-    const moviesArray = movies.data.results.map(movie => new Movie(movie));
-    response.status(200).send(moviesArray);
+
+    const key = searchQuery;
+
+    if (cacheDB[key]) {
+      cacheDB[key].timestamp = Date.now();
+      response.send(cacheDB);
+    }
+    else {
+
+      const movies = await axios.get(url);
+      console.log(movies.data);
+      const moviesArray = movies.data.results.map(movie => new Movie(movie));
+      cacheDB[key] = moviesArray;
+      response.status(200).send(moviesArray);
+
+    }
   } catch (error) {
     next(error);
   }
